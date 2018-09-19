6e19635b36b0c3b02bd286a2327a3cd5e5e5d2b9
lstuckstette <stuckstette@outlook.com>
2018-09-19 16:04:22 +0200

    working polymer template with icons

--- src/main/java/com/volavis/veraplan/spring/views/LoginView.java
+++ src/main/java/com/volavis/veraplan/spring/views/LoginView.java
@@ -1,9 +1,6 @@
 package com.volavis.veraplan.spring.views;
 
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.HtmlContainer;
-import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.dependency.StyleSheet;
@@ -18,11 +15,11 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 @Tag("login-view")
 @HtmlImport("src/login-view.html")
 @Route(value = "login")
-//@StyleSheet("https://www.w3schools.com/w3css/4/w3.css")
-//@StyleSheet("https://www.w3schools.com/lib/w3-theme-blue-grey.css")
+
 public class LoginView extends Component implements HasUrlParameter<String> {
 
     public LoginView() {
+        UI.getCurrent().getPage().addStyleSheet("https://use.fontawesome.com/releases/v5.3.1/css/all.css");
         //init();
         //this.setClassName("w3-container");
     }
