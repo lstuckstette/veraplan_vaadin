ddb6bc1bf6d4a4ab241b943f2c66a7407a3b9fa7
lstuckstette <stuckstette@outlook.com>
2018-10-09 16:06:14 +0200

    landing page (material design+webcomponents) css tweaks

--- src/main/java/com/volavis/veraplan/spring/views/LandingView.java
+++ src/main/java/com/volavis/veraplan/spring/views/LandingView.java
@@ -3,7 +3,9 @@ package com.volavis.veraplan.spring.views;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.page.BodySize;
 import com.vaadin.flow.component.page.Inline;
+import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.component.tabs.Tab;
@@ -16,9 +18,9 @@ import com.vaadin.flow.templatemodel.TemplateModel;
 @PageTitle("LandingView")
 @Tag("landing-view")
 @HtmlImport("landing-view.html")
-//@Inline("semantic-ui-import.html")
+@BodySize()
 @Route("landing")
-public class LandingView extends PolymerTemplate<TemplateModel> implements HasUrlParameter<String> {
+public class LandingView extends PolymerTemplate<TemplateModel> implements HasUrlParameter<String>, PageConfigurator {
 
 
    // @Id("menubar")
@@ -37,4 +39,9 @@ public class LandingView extends PolymerTemplate<TemplateModel> implements HasUr
 
     }
 
+    @Override
+    public void configurePage(InitialPageSettings settings) {
+
+
+    }
 }
