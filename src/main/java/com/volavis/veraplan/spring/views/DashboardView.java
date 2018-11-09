98be324f6b0aa5225cf7de6056c86de121c242ba
lstuckstette <stuckstette@outlook.com>
2018-11-09 15:43:15 +0100

    styling through shared-styles

--- src/main/java/com/volavis/veraplan/spring/views/DashboardView.java
+++ src/main/java/com/volavis/veraplan/spring/views/DashboardView.java
@@ -1,30 +1,20 @@
 package com.volavis.veraplan.spring.views;
 
-import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.polymertemplate.Id;
-import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.templatemodel.TemplateModel;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.theme.material.Material;
 import com.volavis.veraplan.spring.MainLayout;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.security.access.annotation.Secured;
 
 @PageTitle("Dashboard")
 @Route(value = "", layout = MainLayout.class)
 public class DashboardView extends Div {
-
 
 
     public DashboardView() {
