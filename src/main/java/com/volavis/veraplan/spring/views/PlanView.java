a37a4d505421900453069cc5cfc71648e1f9d71b
lstuckstette <stuckstette@outlook.com>
2018-12-04 14:59:02 +0100

    fixed collaboration-toolkit canvas sizing problem

--- src/main/java/com/volavis/veraplan/spring/views/PlanView.java
+++ src/main/java/com/volavis/veraplan/spring/views/PlanView.java
@@ -27,7 +27,7 @@ public class PlanView extends Div {
 
         verticalLayout.add(new H2("Plan"));
 
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 60; i++) {
             verticalLayout.add(new Span("derpy!"));
         }
 
